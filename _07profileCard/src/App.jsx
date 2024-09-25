5a817b17138af54109b1878b011c27265c1c6b1d
shubham4672 <shubham4672.20@gmail.com>
2024-09-25 21:27:41 +0530

    profile card added

---
+++ _07profileCard/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import Star from "./components/Star";
+import "./App.css";
+
+function App() {
+  const [contact, setContact] = useState({
+    firstName: "John",
+    lastName: "Doe",
+    phone: "+1 (719) 555-1212",
+    email: "itsmyrealname@example.com",
+    isFavorite: true,
+  });
+
+  function toggleFavorite() {
+    setContact((prevContact) => {
+      return {
+        ...prevContact,
+        isFavorite: !prevContact.isFavorite,
+      };
+    });
+  }
+
+  return (
+    <main>
+      <article className='card'>
+        <img
+          src='../src/assets/images/user.png'
+          alt=''
+          className='card--image'
+        />
+        <div className='card--info'>
+          <Star isFilled={contact.isFavorite} handleClick={toggleFavorite} />
+          <h2 className='card--name'>
+            {contact.firstName} {contact.lastName}
+          </h2>
+          <p className='card--contact'>{contact.phone}</p>
+          <p className='card--contact'>{contact.email}</p>
+        </div>
+      </article>
+    </main>
+  );
+}
+
+export default App;
---
+++ _07profileCard/src/components/Star.jsx
@@ -0,0 +1,20 @@
+export default function Star(props) {
+  const starIcon = props.isFilled ? "star-filled.png" : "star-empty.png";
+  const buttonLabel = props.isFilled
+    ? "Unmark as favorite"
+    : "Mark as favorite";
+  return (
+    <button
+      onClick={props.handleClick}
+      aria-label={buttonLabel}
+      aria-pressed={props.isFilled}
+      className='card--favorite-button'
+    >
+      <img
+        src={`/src/assets/images/${starIcon}`}
+        alt='star icon.'
+        className='card--favorite'
+      />
+    </button>
+  );
+}
