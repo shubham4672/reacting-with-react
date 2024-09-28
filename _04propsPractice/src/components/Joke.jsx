2d2eba53b96391a22d9086666fc4f351d5efe557
shubham4672 <shubham4672.20@gmail.com>
2024-09-28 23:36:54 +0530

    update conditional rendering

--- _04propsPractice/src/components/Joke.jsx
+++ _04propsPractice/src/components/Joke.jsx
@@ -1,11 +1,17 @@
-import "../App.css";
+import React from "react";
 import PropTypes from "prop-types";
+import "../App.css";
 
 export default function Joke(props) {
+  const [show, setShow] = React.useState(false);
+  function toggle() {
+    setShow((prevState) => !prevState);
+  }
   return (
     <>
       {props.setup && <h3>Setup: {props.setup}</h3>}
-      {props.punchline && <p>Punchline: {props.punchline}</p>}
+      {show && <p>Punchline: {props.punchline}</p>}
+      <button onClick={toggle}>{show ? "Hide" : "Show"} punchline</button>
       <hr />
     </>
   );
