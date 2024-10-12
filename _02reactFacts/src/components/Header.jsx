b16422f93efc87619fccd32efbe736473aaa962e
shubham4672 <shubham4672.20@gmail.com>
2024-10-12 07:40:20 +0530

    facts

--- _02reactFacts/src/components/Header.jsx
+++ _02reactFacts/src/components/Header.jsx
@@ -1,13 +1,19 @@
 import Image from "../assets/react.svg";
 
-export default function Header() {
+export default function Header(props) {
   return (
     <nav className='nav'>
       <div className='logo'>
         <img src={Image} alt='React Logo' />
         <strong>ReactFacts</strong>
       </div>
-      <p>React Course - Project 1</p>
+      <div className='toggler'>
+        <p className='toggler--light'>Light</p>
+        <div className='toggler--slider' onClick={props.toggleDarkMode}>
+          <div className='toggler--slider--circle'></div>
+        </div>
+        <p className='toggler--dark'>Dark</p>
+      </div>
     </nav>
   );
 }
